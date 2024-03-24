dd4a3258ada9506c6e7599020be3501111c0fc7c
Shivam Jaiswal <shivam181106@gmail.com>
2024-03-24 22:23:34 +0530

    Initialised react App

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className="flex justify-center items-center text-zinc-200 text-6xl bg-black h-screen w-screen">JOURNEY AURA</h1>
+    </>
+  )
+}
+
+export default App
