3937f43dac8e95621e45834025b4df7506b56f33
Shivam Jaiswal <shivam181106@gmail.com>
2024-04-07 00:33:53 +0530

    added frontend 4

--- frontend/src/components/Home.tsx
+++ frontend/src/components/Home.tsx
@@ -4,6 +4,7 @@ import { BackgroundBeams } from './ui/background-beams';
 import img1 from '../assets/bg_1.jpg'
 import { useEffect, useRef } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { HeroParallax } from './hero-parallax';
 
 // import { Button } from './ui/moving-border';
 const Home = () => {
@@ -31,18 +32,40 @@ const Home = () => {
       const opacity = 1 - (window.scrollY /k);
       inner.style.opacity = opacity.toString();
       // textEL.style.x=`${-100 + (window.scrollY / k)}px`;
+      const rotationAngle = window.scrollY / 10;
+      const textSize = 100 + window.scrollY / 10;
       textEL.style.transition = "transform 0.1s linear";
-      textEL.style.transform = `translateX(${-(window.scrollY)}px)`;
+      textEL.style.transform = `translateX(${-(window.scrollY)}px) rotate(${-rotationAngle}deg) scale(${textSize / 100})`;
       imgEL.style.transition = "transform 0.1s linear";
-      imgEL.style.transform = `translateX(${(window.scrollY)}px)`;
+      imgEL.style.transform = `translateX(${(window.scrollY)}px) rotate(${rotationAngle}deg)`;
+      const rootEl = document.querySelector<HTMLDivElement>("#inner");
+      if (rootEl) {
+        rootEl.style.overflowY = "hidden";
+      }
+      const text3 = document.querySelector<HTMLParagraphElement>("#text3");
+      if (text3) {
+        const opacity = Math.min(window.scrollY / 400, 1);
+        const fontSize = 40 + window.scrollY / 50;
+        text3.style.opacity = opacity.toString();
+        text3.style.fontSize = `${fontSize}px`;
+        const text3Rect = text3.getBoundingClientRect();
+        const text3Top = text3Rect.top;
+        if (text3Top < text3Rect.height*4 + 8 ) {
+          const opacity1 = Math.min(text3Top/130, 1);
+          const fontSize1 = 40 + text3Top/ 30;
+          text3.style.opacity = opacity1.toString();
+          text3.style.transition = "font-size 0.1s linear";
+          text3.style.fontSize = `${fontSize1}px`;
+        }
+      }
 
     };
     document.addEventListener("scroll", divAnimate);
     return () => document.removeEventListener("scroll", divAnimate);
   },[])
   return (
-    <div className='overflow-x-hidden w-full h-[200vh] flex flex-col'>
-      <motion.div className='flex flex-row overflow-x-hidden' id='inner'>
+    <div className='overflow-x-hidden overflow-y-hidden w-full h-[500vh] flex flex-col'>
+      <motion.div className='flex flex-row overflow-x-hidden overflow-y-hidden' id='inner'>
       <motion.div className="flex flex-col gap-7 items-center justify-center px-4" id='textEl'
             initial={{
               opacity: 0,
@@ -55,9 +78,9 @@ const Home = () => {
             duration: 0.7 // Animation duration
           }}}>
           <p id="text1" className="text-white text-6xl font-mono pb-4">Indian Railways</p>
-          <p id="text2" className="text-white text-2xl text-center ">Build software collaboratively with the power of AI, on any 
+          <p id="text2" className="text-white text-2xl text-center "> Your Premier Railway Ticket Booking Platform Ensuring 
           <br/>
-          device, without spending a second on setup</p>
+          Unwavering Safety, Security, and Punctuality</p>
           <button id="start" onClick={onclickHandler} style={{ boxShadow: "1px 1px 20px blue, -1px -1px 20px blue" }} className="py-3 px-2 text-lg font-semibold rounded-2xl cursor-pointer text-white z-30">Book Now</button>
         </motion.div>
         <motion.div id="imgEl" className="h-lvh w-1/2 flex justify-center items-center pt-16 p-4"
@@ -77,14 +100,110 @@ const Home = () => {
         </motion.div>
       </motion.div>
       <motion.div ref={textRef} style={{ y: y }}>
-        <h1 className='text-white items-center justify-center flex text-3xl'>Hello</h1>
+        <h1 id='text3' className='text-white items-center justify-center flex text-xl'>Features and Services, crafted to cater to your needs.</h1>
       </motion.div>
       <div>
-      <h1 className='text-white items-center justify-center flex text-3xl'>dfhjkljdfzkjldfx</h1>
+        <HeroParallax products={products} />
       </div>
-      <BackgroundBeams className={"h-[200vh]"}/>
+      <BackgroundBeams className={"h-[530vh]"}/>
+
     </div>
   )
 }
 export default Home;
 // export default withSplashScreen(Home);
+export const products = [
+  {
+    title: "Moonbeam",
+    link: "https://gomoonbeam.com",
+    thumbnail:
+      "https://aceternity.com/images/products/thumbnails/new/moonbeam.png",
+  },
+  {
+    title: "Cursor",
+    link: "https://cursor.so",
+    thumbnail:
+      "https://aceternity.com/images/products/thumbnails/new/cursor.png",
+  },
+  {
+    title: "Rogue",
+    link: "https://userogue.com",
+    thumbnail:
+      "https://aceternity.com/images/products/thumbnails/new/rogue.png",
+  },
+ 
+  {
+    title: "Editorially",
+    link: "https://editorially.org",
+    thumbnail:
+      "https://aceternity.com/images/products/thumbnails/new/editorially.png",
+  },
+  {
+    title: "Editrix AI",
+    link: "https://editrix.ai",
+    thumbnail:
+      "https://aceternity.com/images/products/thumbnails/new/editrix.png",
+  },
+  {
+    title: "Pixel Perfect",
+    link: "https://app.pixelperfect.quest",
+    thumbnail:
+      "https://aceternity.com/images/products/thumbnails/new/pixelperfect.png",
+  },
+ 
+  {
+    title: "Algochurn",
+    link: "https://algochurn.com",
+    thumbnail:
+      "https://aceternity.com/images/products/thumbnails/new/algochurn.png",
+  },
+  {
+    title: "Aceternity UI",
+    link: "https://ui.aceternity.com",
+    thumbnail:
+      "https://aceternity.com/images/products/thumbnails/new/aceternityui.png",
+  },
+  {
+    title: "Tailwind Master Kit",
+    link: "https://tailwindmasterkit.com",
+    thumbnail:
+      "https://aceternity.com/images/products/thumbnails/new/tailwindmasterkit.png",
+  },
+  {
+    title: "SmartBridge",
+    link: "https://smartbridgetech.com",
+    thumbnail:
+      "https://aceternity.com/images/products/thumbnails/new/smartbridge.png",
+  },
+  {
+    title: "Renderwork Studio",
+    link: "https://renderwork.studio",
+    thumbnail:
+      "https://aceternity.com/images/products/thumbnails/new/renderwork.png",
+  },
+ 
+  {
+    title: "Creme Digital",
+    link: "https://cremedigital.com",
+    thumbnail:
+      "https://aceternity.com/images/products/thumbnails/new/cremedigital.png",
+  },
+  {
+    title: "Golden Bells Academy",
+    link: "https://goldenbellsacademy.com",
+    thumbnail:
+      "https://aceternity.com/images/products/thumbnails/new/goldenbellsacademy.png",
+  },
+  {
+    title: "Invoker Labs",
+    link: "https://invoker.lol",
+    thumbnail:
+      "https://aceternity.com/images/products/thumbnails/new/invoker.png",
+  },
+  {
+    title: "E Free Invoice",
+    link: "https://efreeinvoice.com",
+    thumbnail:
+      "https://aceternity.com/images/products/thumbnails/new/efreeinvoice.png",
+  },
+];
